a27a943ad932954e257eccb81a9a4ea7c3d41d7a
renatopcinfo <renatopcinfo@gmail.com>
2020-06-25 21:15:45 -0300

    resolvers

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,30 +1,26 @@
 import express from 'express';
-import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
-const server = express();
+const app = express();
 
-
-
-server.get('/status', (_, res) => {
-  res.send({
-    status: 'Ok',
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
 
-const enableCors = cors({ origin: 'http://localhost:3000' })
-
-server
-  .options('/authenticate', enableCors)
-  .post('/authenticate', enableCors, express.json(), (req, res) => {
-    console.log('E-Mail', req.body.email, 'Senha', req.body.password);
-    res.send({
-      Okay: true,
-    });
-  });
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: 'http://localhost:3000'
+  },
+  bodyParserConfig: true,
+});
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
   console.log(`Server is listing at http://${HOSTNAME}:${PORT}`)
 });
