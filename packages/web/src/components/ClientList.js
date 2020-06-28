d1641e870cf9eb778e27367f17d390dbfb0419ba
renatopcinfo <renatopcinfo@gmail.com>
2020-06-28 01:09:39 -0300

    edit clients

--- packages/web/src/components/ClientList.js
+++ packages/web/src/components/ClientList.js
@@ -4,39 +4,31 @@ import { useQuery } from 'react-apollo';
 
 const GET_CLIENT_LIST = gql`
   query GET_CLIENT_LIST($skip: Int!, $take: Int!) {
-    clients(options: {
-      skip: $skip
-      take: $take
-    }) {
+    clients(options: { skip: $skip, take: $take }) {
       items {
         id
         name
         email
       }
-      totalItems       
+      totalItems
     }
   }
 `;
 
 const PAGE_SIZE = 10;
 
-export function ClientList() {
-  const {
-    data,
-    error,
-    loading,
-    fetchMore,
-  } = useQuery(GET_CLIENT_LIST,
-    {
-      fetchPolicy: 'cache-and-network',
-      variables: {
-        skip: 0,
-        take: PAGE_SIZE,
-      }
-    }
-  );
+export function ClientList({ onSelectClient }) {
+  const { data, error, loading, fetchMore } = useQuery(GET_CLIENT_LIST, {
+    fetchPolicy: 'cache-and-network',
+    variables: {
+      skip: 0,
+      take: PAGE_SIZE,
+    },
+  });
 
   const clients = data ?.clients.items ?? [];
+
+  const handleSelectClient = (client) => () => onSelectClient ?.(client.id);
 
   const handleLoadMore = () => {
     fetchMore({
@@ -45,20 +37,19 @@ export function ClientList() {
         take: PAGE_SIZE,
       },
       updateQuery: (result, { fetchMoreResult }) => {
-        if (!fetchMoreResult)
-          return result;
+        if (!fetchMoreResult) return result;
 
         return {
           ...result,
           clients: {
             ...result.clients,
             items: result.clients.items.concat(fetchMoreResult.clients.items),
-            totalItems: fetchMoreResult.clients.totalItems
-          }
-        }
-      }
-    })
-  }
+            totalItems: fetchMoreResult.clients.totalItems,
+          },
+        };
+      },
+    });
+  };
 
   if (error)
     return (
@@ -66,6 +57,7 @@ export function ClientList() {
         <strong>Erro ao buscar os clientes</strong>
       </section>
     );
+
   if (loading && !data)
     return (
       <section>
@@ -77,7 +69,7 @@ export function ClientList() {
     <section>
       <ul>
         {clients.map((client) => (
-          <li key={client.id}>
+          <li key={client.id} onClick={handleSelectClient(client)}>
             <p>{client.name}</p>
             <p>{client.email}</p>
           </li>
